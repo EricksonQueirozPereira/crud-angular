11bc700bf9d7a9f790be8cc84d20585d7ba02e45
ERICKSON GUSTAVO DE QUEIROZ PEREIRA <ericksongustavo2022@gmail.com>
2023-02-17 18:26:38 -0300

    componente router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-{ path:'',pathMatch:'full', redirectTo:'courses'}
+{ path:'',pathMatch:'full', redirectTo:'courses'},
+{
 path: 'courses',
     loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
   }
